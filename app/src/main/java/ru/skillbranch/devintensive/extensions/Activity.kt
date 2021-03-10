4c74ad0be8e534e1bee783c2a21fc46230942124
Vadim Khudozhilov <ccod@mail.ru>
2021-03-10 10:31:32 +0300

    Update Activity.kt

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -7,8 +7,9 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 fun Activity.hideKeyboard() : Unit {
-    var imm: InputMethodManager  = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(currentFocus?.windowToken, InputMethodManager.SHOW_FORCED);
+    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    val targetView = currentFocus ?: View(this)
+    inputMethodManager.hideSoftInputFromWindow(targetView.windowToken, 0)
 }
 
 fun Activity.isKeyboardOpen(): Boolean {
